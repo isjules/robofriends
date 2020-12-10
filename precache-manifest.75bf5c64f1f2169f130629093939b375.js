35248ad5a648000cecac5a51b998f39d38d2753d
Julie Dumbrique <juliedumbrique@Julies-Air.lan1>
2020-12-10 13:33:14 -0800

    Updates

---
+++ precache-manifest.75bf5c64f1f2169f130629093939b375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010191d1e0949c001839f78763691667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e52c20eaf709252e628d",
+    "url": "/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "e266b804babe5454c13b",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "e52c20eaf709252e628d",
+    "url": "/static/js/2.f8c53c40.chunk.js"
+  },
+  {
+    "revision": "cb4738a14a2ef86f47cb194746bb3116",
+    "url": "/static/js/2.f8c53c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e266b804babe5454c13b",
+    "url": "/static/js/main.575b6a54.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
